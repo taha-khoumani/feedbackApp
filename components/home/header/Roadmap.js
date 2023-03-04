48cd597895446f5d91f17ca9585871f73de43e4b
taha-khoumani <khoumanitaha23@gmail.com>
2023-03-04 18:13:10 -0500

    v 3.3

--- components/home/header/Roadmap.js
+++ components/home/header/Roadmap.js
@@ -6,12 +6,24 @@ import Link from "next/link"
 //styles
 import styles from "@/styles/css/header.module.css"
 
+//state
+import { toggleMenu } from '@/state/slices/uiSlice'
+import { useDispatch } from 'react-redux'
+
 export default function Roadmap() {
+
+  const dispatch = useDispatch()
+
   return (
     <div id={styles.roadmap}>
       <div>
         <p>Roadmap</p>
-        <Link href="/roadmap" >View</Link>
+        <Link 
+          href="/roadmap"
+          onClick={()=>{
+              dispatch(toggleMenu(false))
+          }}  
+        >View</Link>
       </div>
       <ul>
         <li>Planned <span>2</span></li>
