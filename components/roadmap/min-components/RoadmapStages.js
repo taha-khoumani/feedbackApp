eb11c5dfd7aa8e622f28434f575f7f1847facf7f
taha-khoumani <khoumanitaha23@gmail.com>
2023-03-04 11:59:22 -0500

    v 3.0

--- components/roadmap/min-components/RoadmapStages.js
+++ components/roadmap/min-components/RoadmapStages.js
@@ -17,15 +17,12 @@ export default function RoadmapStages(props) {
   const dispatch = useDispatch()
 
   function setStage(newStage){
-    dispatch(setcurrentStage(newStage))
+    dispatch(setcurrentStage("caca"))
+    console.log(currentStage)
   }
 
   // dispatch(setcurrentStage("tagopi"))
   function onScroll (id,direction){
-    if(direction === "u" && direction === "d") return
-
-    setStage("Live")
-    console.log(currentStage)
     const navigater = document.querySelector(".roadmap_roadmap_stages__FAUDD")
     const currentPosition = window.getComputedStyle(navigater).left
     let to;
@@ -85,49 +82,80 @@ export default function RoadmapStages(props) {
   useEffect(()=>{
 
     //mobile-scrolling-event-listener
-    function detectswipe(el,func) {
-      var swipe_det = new Object();
-      swipe_det.sX = 0;
-      swipe_det.sY = 0;
-      swipe_det.eX = 0;
-      swipe_det.eY = 0;
-      var min_x = 20;  //min x swipe for horizontal swipe
-      var max_x = 40;  //max x difference for vertical swipe
-      var min_y = 40;  //min y swipe for vertical swipe
-      var max_y = 50;  //max y difference for horizontal swipe
-      var direc = "";
-      var ele = document.getElementById(el);
-      ele.addEventListener('touchstart',function(e){
-        var t = e.touches[0];
-        swipe_det.sX = t.screenX; 
-        swipe_det.sY = t.screenY;
-      },false);
-      ele.addEventListener('touchmove',function(e){
-        e.preventDefault();
-        var t = e.touches[0];
-        swipe_det.eX = t.screenX; 
-        swipe_det.eY = t.screenY;    
-      },false);
-      ele.addEventListener('touchend',function(e){
-        //horizontal detection
-        if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y)))) {
-          if(swipe_det.eX > swipe_det.sX) direc = "r";
-          else direc = "l";
-        }
-        //vertical detection
-        if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x)))) {
-          if(swipe_det.eY > swipe_det.sY) direc = "d";
-          else direc = "u";
-        }
-    
-        if (direc != "") {
-          if(typeof func == 'function') func(el,direc);
-        }
-        direc = "";
-      },false);  
-    }
+    (function(d) {
+      // based on original source: https://stackoverflow.com/a/17567696/334451
+      var newEvent = function(e, name) {
+          // This style is already deprecated but very well supported in real world: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/initCustomEvent
+          // in future we want to use CustomEvent function: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+          var a = document.createEvent("CustomEvent");
+          a.initCustomEvent(name, true, true, e.target);
+          e.target.dispatchEvent(a);
+          a = null;
+          return false
+      };
+      var debug = false; // emit info to JS console for all touch events?
+      var active = false; // flag to tell if touchend should complete the gesture
+      var min_gesture_length = 20; // minimum gesture length in pixels
+      var tolerance = 0.3; // value 0 means pixel perfect movement up or down/left or right is required, 0.5 or more means any diagonal will do, values between can be tweaked
+  
+      var sp = { x: 0, y: 0, px: 0, py: 0 }; // start point
+      var ep = { x: 0, y: 0, px: 0, py: 0 }; // end point
+      var touch = {
+          touchstart: function(e) {
+              active = true;
+              var t = e.touches[0];
+              sp = { x: t.screenX, y: t.screenY, px: t.pageX, py: t.pageY };
+              ep = sp; // make sure we have a sensible end poin in case next event is touchend
+              debug && console.log("start", sp);
+          },
+          touchmove: function(e) {
+              if (e.touches.length > 1) {
+                  active = false;
+                  debug && console.log("aborting gesture because multiple touches detected");
+                  return;
+              }
+              var t = e.touches[0];
+              ep = { x: t.screenX, y: t.screenY, px: t.pageX, py: t.pageY };
+              debug && console.log("move", ep, sp);
+          },
+          touchend: function(e) {
+              if (!active)
+                  return;
+              debug && console.log("end", ep, sp);
+              var dx = Math.abs(ep.x - sp.x);
+              var dy = Math.abs(ep.y - sp.y);
+  
+              if (Math.max(dx, dy) < min_gesture_length) {
+                  debug && console.log("ignoring short gesture");
+                  return; // too short gesture, ignore
+              }
+  
+              if (dy > dx && dx/dy < tolerance && Math.abs(sp.py - ep.py) > min_gesture_length) { // up or down, ignore if page scrolled with touch
+                  newEvent(e, (ep.y - sp.y < 0 ? 'gesture-up' : 'gesture-down'));
+                  //e.cancelable && e.preventDefault();
+              }
+              else if (dx > dy && dy/dx < tolerance && Math.abs(sp.px - ep.px) > min_gesture_length) { // left or right, ignore if page scrolled with touch
+                  newEvent(e, (ep.x - sp.x < 0 ? 'gesture-left' : 'gesture-right'));
+                  //e.cancelable && e.preventDefault();
+              }
+              else {
+                  debug && console.log("ignoring diagonal gesture or scrolled content");
+              }
+              active = false;
+          },
+          touchcancel: function(e) {
+              debug && console.log("cancelling gesture");
+              active = false;
+          }
+      };
+      for (var a in touch) {
+          d.addEventListener(a, touch[a], false);
+          // TODO: MSIE touch support: https://github.com/CamHenlin/TouchPolyfill
+      }
+    })(window.document);
 
-    detectswipe('roadmap_stages',onScroll)
+    document.querySelector('#roadmap_stages').addEventListener('gesture-right',()=>onScroll("roadmap_stages","r"))
+    document.querySelector('#roadmap_stages').addEventListener('gesture-left',()=>onScroll("roadmap_stages","l"))
   },[])
 
   return (
