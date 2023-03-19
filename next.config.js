68f160ceabfaf2852c7f6ea05148e94663fd541b
taha-khoumani <khoumanitaha23@gmail.com>
2023-03-19 16:56:52 -0400

    v7.4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
+const {PHASE_DEVLOPEMENT_SERVER} = require('next/constants')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env:{
-    DATABASE:'feedback',
-    SIGN_OUT_REDIRECT:'http://192.168.2.207:3000/',
+const nextConfig = (phase) =>{
+  if(phase === PHASE_DEVLOPEMENT_SERVER){
+    return {
+      reactStrictMode: true,
+      env:{
+        DATABASE:'feedback',
+        SIGN_OUT_REDIRECT:'http://192.168.2.207:3000/',
+      }
+    }
+  }
+
+  return{
+    reactStrictMode: true,
+    env:{
+      DATABASE:'feedback_production',
+      SIGN_OUT_REDIRECT:'/',
+    }
   }
 }
 
