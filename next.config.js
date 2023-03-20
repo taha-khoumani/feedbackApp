2f764415acea63aeddf2d2b93cc812f29158e977
taha-khoumani <khoumanitaha23@gmail.com>
2023-03-20 00:17:37 -0400

    v7.8

--- next.config.js
+++ next.config.js
@@ -1,26 +1,12 @@
-const {PHASE_DEVLOPEMENT_SERVER} = require('next/constants')
-
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase) => {
-  if(phase === PHASE_DEVLOPEMENT_SERVER){
-    return {
-      reactStrictMode: true,
-      env:{
-        DATABASE:'feedback',
-        SIGN_OUT_REDIRECT:'http://192.168.2.207:3000/',
-        NEXTAUTH_SECRET:'Pnj4s2VIZ5W6U06CmJHN+ASmvKFg2O9HXuxINpnazT4=',
-      }
-    }
+const nextConfig = {
+  reactStrictMode: true,
+  env:{
+    DATABASE:'feedback',
+    SIGN_OUT_REDIRECT:'http://192.168.2.207:3000/',
+    NEXTAUTH_SECRET:'Pnj4s2VIZ5W6U06CmJHN+ASmvKFg2O9HXuxINpnazT4=',
   }
-
-  return {
-    reactStrictMode: true,
-    env:{
-      DATABASE:'feedback_production',
-      SIGN_OUT_REDIRECT:'/',
-      NEXTAUTH_SECRET:'Pnj4s2VIZ5W6U06CmJHN+ASmvKFg2O9HXuxINpnazT4=',
-    }
-  }
+  
 }
 
 module.exports = nextConfig
